6aa2e50a5c14f2036f69dc8b22ec227f60d3bede
lukas <61111761+lukeitslukas@users.noreply.github.com>
2023-01-18 22:24:41 +0000

    BIG WEBSITE UPDATE VERY GOOD
    its so nice

---
+++ assets/js/typed.mjs
@@ -0,0 +1,8 @@
+var options = {
+  strings: ['an Ethical Hacking Student', 'a Programmer', 'an Occasional Streamer', 'looking for a Graduate job!'],
+  backSpeed: 20,
+  typeSpeed: 40,
+  loop: true
+};
+
+var typed = new Typed('#typed', options);
